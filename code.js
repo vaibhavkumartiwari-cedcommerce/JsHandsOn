96efa47894940cad557f328474c1b74800455b22
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 18:06:52 +0530

    Build JavaScript Objects

--- code.js
+++ code.js
@@ -1,33 +1,9 @@
-let count = 0;
-
-function cc(card) {
-  // Only change code below this line
- switch(card){
-   case 2: 
-   case 3:
-   case 4:
-   case 5:
-   case 6:
-        count++;
-   
-        break;
-   case 10:     
-   case 'J': 
-   case 'Q':
-   case 'K':
-   case 'A':
-        count--;
-        break;     
- }
- var result;
- if(count>0)
-    result=' Bet';
- else
-    result=' Hold';   
-
-return count + result;
+const myDog = {
+    // Only change code below this line
+     name : "cat",
+     legs :4,
+     tails:0,
+     friends: ["milk,Mouse"]
   
-  // Only change code above this line
-}
-
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+    // Only change code above this line
+  };
