45aa54dc785df23c14d99864ed74bd5c1e62ea22
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 18:41:12 +0530

    Iterate with JavaScript While Loops

--- code.js
+++ code.js
@@ -1,20 +1,9 @@
-const myPlants = [
-    {
-      type: "flowers",
-      list: [
-        "rose",
-        "tulip",
-        "dandelion"
-      ]
-    },
-    {
-      type: "trees",
-      list: [
-        "fir",
-        "pine",
-        "birch"
-      ]
-    }
-  ];
-  
-  const secondTree = myPlants[1].list[1];
+// Setup
+const myArray = [];
+
+// Only change code below this line
+var i=5;
+while(i>=0){
+  myArray.push(i);
+  i--
+};
