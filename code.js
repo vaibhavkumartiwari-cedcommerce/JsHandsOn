6e38ae860a6931244c8f58ee653d8b5bd0e276a2
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 18:04:02 +0530

    Counting Cards

--- code.js
+++ code.js
@@ -1,13 +1,33 @@
-// Setup
-function abTest(a, b) {
-    // Only change code below this line
-      if(a<0||b<0)
-       return undefined;
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+ switch(card){
+   case 2: 
+   case 3:
+   case 4:
+   case 5:
+   case 6:
+        count++;
+   
+        break;
+   case 10:     
+   case 'J': 
+   case 'Q':
+   case 'K':
+   case 'A':
+        count--;
+        break;     
+ }
+ var result;
+ if(count>0)
+    result=' Bet';
+ else
+    result=' Hold';   
+
+return count + result;
   
-  
-    // Only change code above this line
-  
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
-  }
-  
-  abTest(2,2);
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
