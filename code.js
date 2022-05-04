57a785fe755624037457b5f5186422c269744369
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 14:45:40 +0530

    Find the Length of a String

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
-// Change code below this line
-const someAdjective = "Fun";
-let myStr = "Learning to code is ";
-myStr+=someAdjective;
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
