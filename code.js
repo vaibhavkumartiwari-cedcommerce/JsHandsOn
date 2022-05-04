7c8eebfa8e2acbe38281b0a2471a53ed325598d8
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 17:45:32 +0530

    Replacing If Else Chains with Switch

--- code.js
+++ code.js
@@ -1,32 +1,29 @@
-function sequentialSizes(val) {
+function chainToSwitch(val) {
     let answer = "";
     // Only change code below this line
-  
-    switch(val){
-           case 1: answer="Low";
-           break;
-           case 2: answer="Low";
-           break;
-           case 3: answer="Low";
-           break;
-           case 4: answer="Mid";
-           break;
-            case 5: answer="Mid";
-           break;
-            case 6: answer="Mid";
-           break;
-           case 7: answer="High";
-           break;
-            case 8: answer="High";
-           break; 
-           case 9: answer="High";
-           break;
-  
-          
-         }
-  
+  switch(val){
+    case "bob":
+      answer = "Marley";
+       break;
+       case 42:
+      answer = "The Answer";
+       break;
+       case 1:
+       answer = "There is no #1";
+       break;
+       case 99 :
+       answer = "Missed me by this much!";
+       break;
+       case 7:
+        answer = "Ate Nine";
+        break;
+    
+    
+      
+    
+  }
     // Only change code above this line
     return answer;
   }
   
-  sequentialSizes(1);
+  chainToSwitch(7);
