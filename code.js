745dd72dc0b26700dfa09cc84b31d170d4a74689
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 17:50:16 +0530

    Return Early Pattern for Functions

--- code.js
+++ code.js
@@ -1,7 +1,13 @@
-function isLess(a, b) {
+// Setup
+function abTest(a, b) {
     // Only change code below this line
-    return b>=a;
+      if(a<0||b<0)
+       return undefined;
+  
+  
     // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
   }
   
-  isLess(10, 15);
+  abTest(2,2);
