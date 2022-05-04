869cf51ef764b6117a10780746e2d516d56aebc4
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 18:15:58 +0530

    Add New Properties to a JavaScript Object

--- code.js
+++ code.js
@@ -1,10 +1,8 @@
-// Setup
 const myDog = {
-    "name": "Coder",
+    "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
     "friends": ["freeCodeCamp Campers"]
   };
+  myDog.bark="woof";
   
-  // Only change code below this line
-  myDog.name="Happy Coder";
