8054f69a27bb18981714d88ce1d00f6d288305e1
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 16:47:13 +0530

    Stand in Line

--- code.js
+++ code.js
@@ -1,9 +1,18 @@
-// Setup
-let processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
-}
-
-// Only change code below this line
-processed=processArg(7);
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    var p=arr.shift();
+    
+  
+    
+    return p;
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
