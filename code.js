77b02718333e5187b1a486117df8e67e80bb67e0
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 18:59:10 +0530

    Profile Lookup

--- code.js
+++ code.js
@@ -1,14 +1,46 @@
-
-  // Only change code below this line
-  function sum(arr, n) {
-    if(n <= 0) {
-      return 0;
-    } else {
-      return sum(arr, n - 1) + arr[n - 1];
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  
+    // Only change code below this line
+    function lookUpProfile(name, prop) {
+    for (let i= 0; i < contacts.length; i++) {
+      if (contacts[i].firstName === name) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          return contacts[i][prop];
+        } else {
+          return "No such property";
+        }
+      }
     }
+    return "No such contact";
+  
+    // Only change code above this line
   }
   
-        
-          
-    // Only change code above this line
-  
+  lookUpProfile("Akira", "likes");
