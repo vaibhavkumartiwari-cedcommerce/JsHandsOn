bc61869265841809eb7db5e954085e2fe57e1136
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 16:39:42 +0530

    Local Scope and Functions

--- code.js
+++ code.js
@@ -1,21 +1,11 @@
-// Declare the myGlobal variable below this line
-let myGlobal=10;
-
-function fun1() {
-  // Assign 5 to oopsGlobal Here
-  oopsGlobal=5;
-
-}
-
-// Only change code above this line
-
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
+function myLocalScope() {
+    // Only change code below this line
+    let myVar;
+  
+    console.log('inside myLocalScope', myVar);
   }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
