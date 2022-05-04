8bb8a323b68b9537fb1e3a818c906e639f484a94
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 19:18:09 +0530

    ternary

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
-function checkEqual(a, b) {
-    return a==b?"Equal":"Not Equal";
- }
- 
- checkEqual(1, 2);
+function checkSign(num) {
+    return num > 0 ? "positive"
+      : num < 0 ? "negative"
+      : "zero";
+  
+  }
+  
+  checkSign(10);
