a9c4fb01c61562e79e7112fe429e229f8e6f73a6
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 18:08:03 +0530

    Accessing Object Properties with Dot Notation

--- code.js
+++ code.js
@@ -1,9 +1,10 @@
-const myDog = {
-    // Only change code below this line
-     name : "cat",
-     legs :4,
-     tails:0,
-     friends: ["milk,Mouse"]
-  
-    // Only change code above this line
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
   };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
