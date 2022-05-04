4435bcd3941407fb2c982697ca143c0373f03209
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 16:10:21 +0530

    Store Multiple Values in one Variable using JavaScript Arrays

--- code.js
+++ code.js
@@ -1,5 +1,2 @@
-// Setup
-const lastName = "Lovelace";
-
 // Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+const myArray = ["HEllo",5];
