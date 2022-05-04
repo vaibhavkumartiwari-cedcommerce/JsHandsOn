51af05b57beedf8fa9870613061fe3285021354a
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 14:42:35 +0530

    Constructing Strings with Variables

--- code.js
+++ code.js
@@ -1,3 +1,3 @@
-let myStr="This is the first sentence.";
-myStr+= " This is the second sentence.";
-
+// Only change code below this line
+const myName = "Vaibhav";
+const myStr = "My name is "+myName+ "and i am well";
