c5110d996210246c4438e75545e15f22d8b4e555
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 19:21:04 +0530

    last Task

--- code.js
+++ code.js
@@ -1,12 +1,9 @@
-// Only change code below this line
-
-function countdown(n){
-    if (n < 1) {
-      return [];
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum == 0) {
+      return [startNum];
     } else {
-      const a = countdown(n - 1);
-      a.unshift(n);
-      return a;
+      var res = rangeOfNumbers(startNum, endNum - 1);
+      res.push(endNum);
+      return res;
     }
-  }
-  // Only change code above this line
+  };
