d36f73d0c0a3264e3ef6b12ca251e265990d0d54
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 16:52:59 +0530

    Comparison with the Equality Operator

--- code.js
+++ code.js
@@ -1,11 +1,9 @@
-function trueOrFalse(wasThatTrue) {
-    // Only change code below this line
-  
-     if(wasThatTrue)
-        return "Yes, that was true";
-     else
-        return "No, that was false";   
-  
-    // Only change code above this line
-  
+// Setup
+function testEqual(val) {
+    if (val==12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
   }
+  
+  testEqual(10);
