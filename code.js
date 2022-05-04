c346f4e04a978a50136cdad3ba8b57dbdec6a818
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 18:23:40 +0530

    Testing Objects for Properties

--- code.js
+++ code.js
@@ -1,21 +1,8 @@
-// Setup
-function phoneticLookup(val) {
-    let result = "";
-  
+function checkObj(obj, checkProp) {
     // Only change code below this line
-    const obj ={
-      alpha:"Adams",
-      bravo:"Boston",
-      charlie:"Chicago",
-      delta:"Denver",
-      echo:"Easy",
-      foxtrot:"Frank"
-    };
-  
-   result=obj[val];
-  
+    if(obj[checkProp])
+    return obj[checkProp];
+    else
+     return "Not Found";
     // Only change code above this line
-    return result;
   }
-  
-  phoneticLookup("charlie");
